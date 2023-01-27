a21141310145bf797aa9835e3640897139d17a76
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-01-27 21:55:58 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+const { Component } = require('react');
+
+class App extends Component {
+  state = {
+    items: [],
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
+
+export default App;
